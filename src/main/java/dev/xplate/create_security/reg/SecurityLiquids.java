f1716d0c1b6c91ec56934b01e6ad4cc23231fa1b
_FN10_ <mathdudex09@gmail.com>
2026-06-27 22:28:34 -0300

    fix liquid finiranium

--- src/main/java/dev/xplate/create_security/reg/SecurityLiquids.java
+++ src/main/java/dev/xplate/create_security/reg/SecurityLiquids.java
@@ -7,19 +7,27 @@ import com.tterrag.registrate.builders.FluidBuilder;
 import com.tterrag.registrate.util.entry.FluidEntry;
 import dev.xplate.create_security.blocks.FiniraniumRelatedFluidBlock;
 import dev.xplate.create_security.items.FiniraniumRelatedBucketItem;
+import net.createmod.catnip.theme.Color;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.dispenser.BlockSource;
 import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
 import net.minecraft.core.dispenser.DispenseItemBehavior;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.DispensibleContainerItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.MapColor;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.fluids.BaseFlowingFluid;
+import net.neoforged.neoforge.fluids.FluidStack;
+import org.joml.Vector3f;
+
+import java.util.function.Supplier;
 
 import static dev.xplate.create_security.CSecurity.REG;
 import static dev.xplate.create_security.CSecurity.res;
@@ -27,19 +35,18 @@ import static dev.xplate.create_security.CSecurity.res;
 public class SecurityLiquids {
 
     public static final FluidEntry<BaseFlowingFluid.Flowing> LIQUID_FINIRANIUM =
-            REG.fluid("liquid_finranium",
-                            res("block/fluid/liquid_finiranium_still"),
-                            res("block/fluid/liquid_finiranium_flow")
+            REG.standardFluid("liquid_finiranium",
+                            SolidRenderedPlaceableFluidType.create(0x731e6d, () -> 0.1f)
                     )
                     .lang("Liquid Finiranium")
-                    .properties(b -> b.viscosity(4000)
-                            .density(400))
+                    .properties(b -> b.viscosity(10000)
+                            .density(8000).lightLevel(2).canSwim(false).canDrown(false).temperature(2000))
                     .fluidProperties(p -> p.levelDecreasePerBlock(3)
-                            .tickRate(50)
+                            .tickRate(70)
                             .slopeFindDistance(2)
                             .explosionResistance(500f))
                     .source(BaseFlowingFluid.Source::new) // TODO: remove when Registrate fixes FluidBuilder
-                    .block( (f,p) -> new FiniraniumRelatedFluidBlock(f,p,10))
+                    .block( (f,p) -> new FiniraniumRelatedFluidBlock(f,p,5))
                     .properties(p -> p.mapColor(MapColor.COLOR_MAGENTA))
                     .build()
                     .bucket(FiniraniumRelatedBucketItem::new)
@@ -69,4 +76,52 @@ public class SecurityLiquids {
     public static void reg() {
 
     }
+
+    //taken from create
+    private static class SolidRenderedPlaceableFluidType extends AllFluids.TintedFluidType {
+
+        private Vector3f fogColor;
+        private Supplier<Float> fogDistance;
+
+        public static FluidBuilder.FluidTypeFactory create(int fogColor, Supplier<Float> fogDistance) {
+            return (p, s, f) -> {
+                SolidRenderedPlaceableFluidType fluidType = new SolidRenderedPlaceableFluidType(p, s, f);
+                fluidType.fogColor = new Color(fogColor, false).asVectorF();
+                fluidType.fogDistance = fogDistance;
+                return fluidType;
+            };
+        }
+
+        private SolidRenderedPlaceableFluidType(Properties properties, ResourceLocation stillTexture,
+                                                ResourceLocation flowingTexture) {
+            super(properties, stillTexture, flowingTexture);
+        }
+
+        @Override
+        protected int getTintColor(FluidStack stack) {
+            return NO_TINT;
+        }
+
+        /*
+         * Removing alpha from tint prevents optifine from forcibly applying biome
+         * colors to modded fluids (this workaround only works for fluids in the solid
+         * render layer)
+         */
+        @Override
+        public int getTintColor(FluidState state, BlockAndTintGetter world, BlockPos pos) {
+            return 0x00ffffff;
+        }
+
+        @Override
+        protected Vector3f getCustomFogColor() {
+            return fogColor;
+        }
+
+        @Override
+        protected float getFogDistanceModifier() {
+            return fogDistance.get();
+        }
+
+    }
+
 }
